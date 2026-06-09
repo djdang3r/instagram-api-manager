02e89fd0af77b6ebfd7659e7c295bf40b1bdb061
c_torres <vientoquesurcalosmares@gmail.com>
2026-06-09 11:51:02 -0600

    Corrijo problema de clase repetida!

--- src/Traits/ValidatesHubSignature.php
+++ src/Traits/ValidatesHubSignature.php
@@ -4,7 +4,6 @@ namespace ScriptDevelop\InstagramApiManager\Traits;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Log;
 
 trait ValidatesHubSignature
 {
